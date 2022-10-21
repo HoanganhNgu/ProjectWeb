04e7d87c3d11d63de3fffa6c027ea1593b7301de
hoàng anh nguyễn <hoanganhnguyen31122002@gmail.com>
2022-10-21 11:27:06 +0700

    blabla

---
+++ ProjectWeb-master/project/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project.Data;
+using project.Models;
+
+namespace project.Controllers
+{
+
+    public class OrderController : Controller
+    {
+        protected ApplicationDbContext context;
+
+        public OrderController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult MakeOrder(int book, int stock)
+        {
+            var order = new Order();
+            order.OrderStock = book;
+            order.BookId = book;
+            context.Orders.Add(order);
+            context.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ ProjectWeb-master/project/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace project.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Customer { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int OrderStock { get; set; }
+        public double OrderPrice { get; set; }
+
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+    }
+}
+
